d53eca97f97a47b6cd291d0e88e51dba36b1a940
Marco Marchini <775639+marcom04@users.noreply.github.com>
2023-01-09 22:04:03 +0100

    mysite: rename things, add recommendations.

--- ch01-02-03-blog/mysite/blog/models.py
+++ ch01-02-03-blog/mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -11,13 +12,12 @@ class PublishedManager(models.Manager):
 
 
 class Post(models.Model):
-
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -44,3 +44,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post-detail',
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
--- ch01/mysite/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'blog'
-
-urlpatterns = [
-    path('', views.post_list, name='post-list'),
-    path('<int:id>/', views.post_detail, name='post-detail'),
-]
