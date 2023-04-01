66a50c5abcdd77e6764ec9e97744d2fde7450858
Marco Marchini <775639+marcom04@users.noreply.github.com>
2023-04-01 15:23:43 +0200

    Add follow system.

--- ch04-05-06-07-social/bookmarks/account/views.py
+++ ch04-05-06-07-social/bookmarks/account/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import (
+    render,
+    get_object_or_404,
+)
+from django.http import (
+    HttpResponse,
+    JsonResponse,
+)
 from django.contrib.auth import (
     authenticate,
     login,
+    get_user_model,
 )
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 
 from .forms import (
@@ -13,7 +21,12 @@ from .forms import (
     UserEditForm,
     ProfileEditForm,
 )
-from .models import Profile
+from .models import (
+    Profile,
+    Contact,
+)
+
+User = get_user_model()
 
 
 @login_required
@@ -108,3 +121,57 @@ def edit(request):
             'profile_form': profile_form
         }
     )
+
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(
+        request,
+        'account/user/list.html',
+        {
+            'section': 'people',
+            'users': users
+        }
+    )
+
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(
+        User,
+        username=username,
+        is_active=True
+    )
+    return render(
+        request,
+        'account/user/detail.html',
+        {
+            'section': 'people',
+            'user': user
+        }
+    )
+
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objets.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user
+                )
+            else:
+                Contact.objects.filter(
+                    user_from=request.user,
+                    user_to=user
+                ).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+    return JsonResponse({'status': 'error'})
