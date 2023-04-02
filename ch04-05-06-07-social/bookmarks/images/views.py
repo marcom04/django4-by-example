ce279f2a9742fdca116cd84c92011a429ee1f308
Marco Marchini <775639+marcom04@users.noreply.github.com>
2023-04-02 14:29:49 +0200

    Add redis.

--- ch04-05-06-07-social/bookmarks/images/views.py
+++ ch04-05-06-07-social/bookmarks/images/views.py
@@ -1,3 +1,4 @@
+import redis
 from django.shortcuts import (
     render,
     redirect,
@@ -13,10 +14,17 @@ from django.core.paginator import (
     EmptyPage,
     PageNotAnInteger,
 )
-
+from django.conf import settings
+from actions.utils import create_action
 from .forms import ImageCreateForm
 from .models import Image
-from actions.utils import create_action
+
+# connect to redis
+r = redis.Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB
+)
 
 
 @login_required
@@ -46,12 +54,17 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    # increment total image views by 1
+    total_views = r.incr(f'image:{image.id}:views')
+    # increment image ranking by 1
+    r.zincrby('image_ranking', 1, image.id)
     return render(
         request,
         'images/image/detail.html',
         {
             'section': 'images',
-            'image': image
+            'image': image,
+            'total_views': total_views
         }
     )
 
@@ -102,3 +115,23 @@ def image_list(request):
                   'images/image/list.html',
                    {'section': 'images',
                     'images': images})
+
+
+@login_required
+def image_ranking(request):
+    # get image ranking dictionary
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # get most viewed images
+    most_viewed = list(Image.objects.filter(
+        id__in=image_ranking_ids
+    ))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(
+        request,
+        'images/image/ranking.html',
+        {
+            'section': 'images',
+            'most_viewed': most_viewed
+        }
+    )
